c52a00d67ad1e986ea0ab8922a00fd1bddb89b47
Kamil <90562445+kbayazitov@users.noreply.github.com>
2024-05-06 00:16:13 +0300

    Update train.py

--- src/mylib/train.py
+++ src/mylib/train.py
@@ -439,3 +439,50 @@ def plot_test_results(model, test_dataset, input_size=30, output_size=10, num_ro
         ax[i].plot(np.linspace(input_size, input_size+output_size+1, output_size+1), [x_input[0,-1,3].item()]+pred[0].tolist(), color='r')
 
     plt.show()
+
+def get_datasets(data, iw=30, ow=1, s=1, nf=5, a=0, b=1):
+    train_datasets = []
+    test_datasets = []
+
+    for method in ('base', 'noise', 'auto'):
+        data[f'value_{method}_diff_1'] = data[f'value_{method}'] - data[f'value_{method}'].shift(shift1)
+        data[f'value_{method}_diff_2'] = data[f'value_{method}_diff_1'] - data[f'value_{method}_diff_1'].shift(shift2)
+
+        data['combined'] = data.apply(lambda x: list([x['open_diff_2'],
+                                                  x['high_diff_2'],
+                                                  x['low_diff_2'],
+                                                  x['close_diff_2'],
+                                                  x[f'value_{method}_diff_2'],
+                                                 ]), axis=1)
+
+        y = np.array(data['combined'].tolist())[shift1+shift2:]
+        t = np.array(data['time'])[shift1+shift2:]
+
+        if method != 'base':
+            num = 5
+        else:
+            num = 4
+
+        for i in range(num):
+            y[:, i] = ((y[:, i] - np.min(y[:, i])) * (b - a) / (np.max(y[:, i]) - np.min(y[:, i]))) + a
+
+        t_train, y_train, t_test, y_test = train_test_split(t, y, split=0.75)
+
+        Xtrain, Ytrain = windowed_dataset(y_train, input_window=iw, output_window=ow, stride=s, num_features=nf)
+        Xtest, Ytest = windowed_dataset(y_test, input_window=iw, output_window=ow, stride=s, num_features=nf)
+        X_train, Y_train, X_test, Y_test = numpy_to_torch(Xtrain, Ytrain, Xtest, Ytest)
+
+        train_datasets.append(torch.utils.data.TensorDataset(X_train, Y_train))
+        test_datasets.append(torch.utils.data.TensorDataset(X_test, Y_test))
+        '''
+        if method == 'base':
+            dataset_train_pt_base = torch.utils.data.TensorDataset(X_train, Y_train)
+            dataset_test_pt_base = torch.utils.data.TensorDataset(X_test, Y_test)
+        elif method == 'noise':
+            dataset_train_pt_noise = torch.utils.data.TensorDataset(X_train, Y_train)
+            dataset_test_pt_noise = torch.utils.data.TensorDataset(X_test, Y_test)
+        elif method == 'auto':
+            dataset_train_pt_auto = torch.utils.data.TensorDataset(X_train, Y_train)
+            dataset_test_pt_auto = torch.utils.data.TensorDataset(X_test, Y_test)
+        '''
+    return train_datasets, test_datasets

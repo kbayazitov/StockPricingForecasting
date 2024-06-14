4496aa8218185525ef5a282e7b318a26fb171e7a
Kamil <90562445+kbayazitov@users.noreply.github.com>
2024-06-14 23:56:07 +0300

    Update lstm.py

--- src/mylib/lstm.py
+++ src/mylib/lstm.py
@@ -8,14 +8,6 @@ class Encoder(nn.Module):
         return next(self.parameters()).device
 
     def __init__(self, input_size, hidden_size, num_layers, dropout, bidirectional):
-
-        '''
-        : param input_size:     the number of features in the input X
-        : param hidden_size:    the number of features in the hidden state h
-        : param num_layers:     number of recurrent layers (i.e., 2 means there are
-        :                       2 stacked LSTMs)
-        '''
-
         super(Encoder, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -27,12 +19,6 @@ class Encoder(nn.Module):
 
     def forward(self, input):
 
-        '''
-        : param x_input:               input of shape (seq_len, # in batch, input_size)
-        : return lstm_out, hidden:     lstm_out gives all the hidden states in the sequence;
-        :                              hidden gives the hidden state and cell state for the last
-        :                              element in the sequence
-        '''
         _, hidden = self.lstm(input)
 
         return _, hidden
@@ -43,13 +29,6 @@ class Decoder(nn.Module):
         return next(self.parameters()).device
 
     def __init__(self, input_size, hidden_size, num_layers, dropout, bidirectional):
-
-        '''
-        : param input_size:     the number of features in the input X
-        : param hidden_size:    the number of features in the hidden state h
-        : param num_layers:     number of recurrent layers (i.e., 2 means there are
-        :                       2 stacked LSTMs)
-        '''
 
         super(Decoder, self).__init__()
         self.input_size = input_size
@@ -63,15 +42,6 @@ class Decoder(nn.Module):
 
     def forward(self, input, encoder_hidden):
 
-        '''
-        : param x_input:                    should be 2D (batch_size, input_size)
-        : param encoder_hidden_states:      hidden states
-        : return output, hidden:            output gives all the hidden states in the sequence;
-        :                                   hidden gives the hidden state and cell state for the last
-        :                                   element in the sequence
-
-        '''
-
         output, hidden = self.lstm(input, encoder_hidden)
         output = self.linear(output)
 
@@ -83,11 +53,6 @@ class Seq2SeqLSTM(nn.Module):
         return next(self.parameters()).device
 
     def __init__(self, input_size, hidden_size, num_layers=1, dropout=0.4, bidirectional=False, teacher_forcing_ratio=0, target_len=10):
-
-        '''
-        : param input_size:     the number of expected features in the input X
-        : param hidden_size:    the number of features in the hidden state h
-        '''
 
         super(Seq2SeqLSTM, self).__init__()
 
@@ -105,16 +70,8 @@ class Seq2SeqLSTM(nn.Module):
 
     def forward(self, input, true_input=None):
 
-        '''
-        : param input_tensor:      input data (seq_len, input_size); PyTorch tensor
-        : param target_len:        number of target values to predict
-        : return np_outputs:       np.array containing predicted values; prediction done recursively
-        '''
-
         batch_size = input.size(0)
 
-        # encode input_tensor
-        #input = input.unsqueeze(1)     # add in batch size of 1 [batch_size,]
         _, encoder_hidden = self.encoder(input) # [batch_size, input_size, hidden_size]
 
         #decoder_input = input
@@ -122,7 +79,6 @@ class Seq2SeqLSTM(nn.Module):
 
         # initialize tensor for predictions
         outputs = torch.zeros(len(input), self.target_len, 1).to(self.device) # [batch_size, target_len, input_size]
-
 
         if self.bidirectional:
             hidden = decoder_hidden[0].view(self.num_layers, 2, batch_size, self.hidden_size)
@@ -132,11 +88,7 @@ class Seq2SeqLSTM(nn.Module):
 
             decoder_hidden = (hidden, cell)
 
-        # decode input_tensor
         decoder_input = input[:, -1, :].view(input.shape[0], 1, input.shape[2])
-
-        #decoder_input = outputs[:, 0, :].view(input.shape[0], 1, input.shape[2])
-        #decoder_input = torch.zeros(len(input), 1).view(input.shape[0], 1, input.shape[2]).to(device)
 
         for t in range(self.target_len):
             decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
